9fda9eb032298da72ca83436c25c3957f6a0a583
scornejom <sergio.cornejo@agrovisioncorp.com>
2024-11-04 10:01:32 -0500

    Actualizacion QAS

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -184,7 +184,7 @@ class _LoginPageState extends State<LoginPage> {
                                         textAlign: TextAlign.center,
                                       ),
                                     ),
-                                    Text("2.0.4",
+                                    Text("2.0.5",
                                         style: TextStyle(
                                             color: Theme.of(context)
                                                 .colorScheme
@@ -308,7 +308,7 @@ class _LoginPageState extends State<LoginPage> {
                                   textAlign: TextAlign.center,
                                 ),
                               ),
-                              Text("2.0.4",
+                              Text("2.0.5",
                                   style: TextStyle(
                                       color: Theme.of(context)
                                           .colorScheme
