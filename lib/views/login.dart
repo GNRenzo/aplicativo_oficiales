4e962a40f08337a2fdaaa06f84af3bc942bd8b78
scornejom <sergio.cornejo@agrovisioncorp.com>
2025-03-19 16:21:42 -0500

    actualization apk 2.1.4

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -184,7 +184,7 @@ class _LoginPageState extends State<LoginPage> {
                                         textAlign: TextAlign.center,
                                       ),
                                     ),
-                                    Text("2.1.3",
+                                    Text("2.1.4",
                                         style: TextStyle(
                                             color: Theme.of(context)
                                                 .colorScheme
@@ -308,7 +308,7 @@ class _LoginPageState extends State<LoginPage> {
                                   textAlign: TextAlign.center,
                                 ),
                               ),
-                              Text("2.1.3",
+                              Text("2.1.4",
                                   style: TextStyle(
                                       color: Theme.of(context)
                                           .colorScheme
